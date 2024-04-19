0b1acdb5d588517ffb3c15c1baff775b8202d7ed
Pedro Rosado <rondaplay@hotmail.com>
2024-04-19 17:18:31 +0200

    javadoc

--- Practica_Algoritmos/src/util/Algoritmos.java
+++ Practica_Algoritmos/src/util/Algoritmos.java
@@ -2,21 +2,36 @@ package util;
 
 public abstract class Algoritmos {
 
-	public static int fibonacci(int numero) {
+	/**
+     * Calcular Fibonacci de un número entero.
+     * @param numero El número entero del que se va a calcular fibonacci.
+     * @return El número de Fibonacci.
+     */
+    public static int fibonacci(int numero) {
         if (numero <= 1)
             return numero;
         else
             return fibonacci(numero - 1) + fibonacci(numero - 2);
     }
-	
-	public static int factorial(int numero) {
+
+    /**
+     * Calcula factorial de un número entero.
+     * @param numero El número entero del cual se va a calcular el factorial.
+     * @return El factorial del número.
+     */
+    public static int factorial(int numero) {
         if (numero == 0)
             return 1;
         else
             return numero * factorial(numero - 1);
     }
-	
-	public static boolean primo(int numero) {
+
+    /**
+     * Verifica si un número entero es primo.
+     * @param numero El número entero.
+     * @return true si el número es primo, false si no lo es.
+     */
+    public static boolean esPrimo(int numero) {
         if (numero <= 1)
             return false;
         for (int i = 2; i <= Math.sqrt(numero); i++) {
